3c2384f42ddbd4495764675b6af0267833640016
monashakeri <mona.shakeri@gmail.com>
2026-09-05 16:03:06 +0330

    Update package config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import { resolve } from 'path'
 
 export default defineConfig({
+  plugins: [vue()],
 
-  plugins: [
-    vue()
-  ]
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'VueSmartTreeSelect',
+      formats: ['es', 'umd'],
+      fileName: (format) => `vue-smart-tree-select.${format}.js`,
+    },
 
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
